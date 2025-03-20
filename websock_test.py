b7757df4ab87241b5c153c4f1eb273457fb2006d
Ju Wang <jwang@vsu.edu>
2025-03-20 16:14:08 -0400

    update

--- websock_test.py
+++ websock_test.py
@@ -1,5 +1,6 @@
 import asyncio
 import websockets
+import ssl
 
 async def test_websocket():
     uri = "wss://service.zenimotion.com/nats"  # Replace with any other WebSocket server if needed
@@ -7,9 +8,10 @@ async def test_websocket():
     #uri = "wss://ws.ifelse.io"  # Replace with any other WebSocket server if needed
     #websocket = new WebSocket("wss://service.zenimotion.com/nats");
     #websocket = new WebSocket("wss://echo.websocket.org");
-
+    ssl_context = ssl._create_unverified_context()  # Bypass SSL verification
+    #async with websockets.connect(uri, ssl=ssl_context) as websocket:
     async with websockets.connect(uri) as websocket:
-        await websocket.send("Hello, WebSocket!")
+        await websocket.send("PUB test.subject 5\r\nHello\r\n")
         response = await websocket.recv()
         print("Received:", response)
 
