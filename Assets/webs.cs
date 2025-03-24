9a7e98cd74aae04d8d36b1510bfc6844d7049271
unknown <collindg06@gmail.com>
2025-03-24 17:02:33 -0400

    update

--- Assets/webs.cs
+++ Assets/webs.cs
@@ -20,10 +20,10 @@ public class Connection : MonoBehaviour
   {
       if (websocket != null && websocket.State == WebSocketState.Open)
       {
-          // Format the NATS publish message: PUB <subject> <bytes>\r\n<messageBody>\r\n
-          string message = $"PUB {subject} {Encoding.UTF8.GetByteCount(jsonMessage)}\r\n{jsonMessage}\r\n";
+           //Format the NATS publish message: PUB <subject> <bytes>\r\n<messageBody>\r\n
+          string message = $"PUB subject.pose {Encoding.UTF8.GetByteCount(jsonMessage)}\r\n{jsonMessage}\r\n";
 
-          Debug.Log($"Sending: {message}");
+          Debug.Log($"Sending: {message}"); 
 
           await websocket.SendText(message);
       }
@@ -40,13 +40,14 @@ public class Connection : MonoBehaviour
   {
     Debug.Log("Starting Connection script on " + gameObject.name);
     ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
-    //websocket = new WebSocket("wss://service.zenimotion.com/nats");
+    websocket = new WebSocket("wss://service.zenimotion.com/nats");
     //websocket = new WebSocket("wss://echo.websocket.org");
-    websocket = new WebSocket("ws://134.209.218.187:8081/nats");
+    //websocket = new WebSocket("ws://134.209.218.187:8081/nats");
 
     websocket.OnOpen += () =>
     {
       Debug.Log("Connection open!");
+      SendWebSocketMessage();
     };
 
     websocket.OnError += (e) =>
@@ -70,11 +71,11 @@ public class Connection : MonoBehaviour
       // Debug.Log("OnMessage! " + message);
     };
 
+    // Keep sending messages at every 0.3s
+    InvokeRepeating("SendWebSocketMessage", 0.0f, 0.3f);
     // waiting for messages
     await websocket.Connect();
-    // Keep sending messages at every 0.3s
-    //InvokeRepeating("SendWebSocketMessage", 0.0f, 0.3f);
-
+    
 
   }
 
@@ -85,21 +86,21 @@ public class Connection : MonoBehaviour
     #endif
   }
 
-  //async void SendWebSocketMessage()
-  //{
-    //byte[] messageBytes = Encoding.UTF8.GetBytes("PUB test.subject 5\r\nHello\r\n");
-    //Debug.Log("Number of bytes: " + messageBytes.Length);
-    //if (websocket.State == WebSocketState.Open)
-    //{
-      // Sending bytes
+  async void SendWebSocketMessage()
+  {
+    byte[] messageBytes = Encoding.UTF8.GetBytes("PUB test.subject 5\r\nHello\r\n");
+    Debug.Log("Number of bytes: " + messageBytes.Length);
+    if (websocket.State == WebSocketState.Open)
+    {
+       //Sending bytes
       //await websocket.Send(new byte[] { 10, 20, 30 });
 
       // Sending plain text
-      //await websocket.SendText("PUB test.subject 5\r\nHello\r\n");
+      await websocket.SendText("PUB subject.pose 5\r\nHello\r\n");
       
       //await websocket.SendText(“PUB jointValues xxx\r\nyyy\r\n”);
-    //}
-  //}
+    }
+  }
 
   private async void OnApplicationQuit()
   {
